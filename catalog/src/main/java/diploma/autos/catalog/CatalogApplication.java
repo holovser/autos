e2632d9d5ffe9a85b105b2eaab6ef8179d039850
Serhii <holovser@gmail.com>
2020-06-03 23:17:05 +0200

    Change eureka client annotation

--- catalog/src/main/java/diploma/autos/catalog/CatalogApplication.java
+++ catalog/src/main/java/diploma/autos/catalog/CatalogApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class CatalogApplication {
 
 	public static void main(String[] args) {
