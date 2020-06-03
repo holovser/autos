4402d686b1ba048725ceee96239ba506fcc9eb3c
Serhii <holovser@gmail.com>
2020-06-03 23:18:09 +0200

    Add eureka client annotation

--- similarity/src/main/java/diploma/autos/similarity/SimilarityApplication.java
+++ similarity/src/main/java/diploma/autos/similarity/SimilarityApplication.java
@@ -2,8 +2,10 @@ package diploma.autos.similarity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SimilarityApplication {
 
 	public static void main(String[] args) {
